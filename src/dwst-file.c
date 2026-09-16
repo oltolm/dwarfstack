oltolm/dwarfstack

commit 395411bea8df9a07736cd73e390f9f845e21a1e0

ignore ranges where lowpc is 0

src/dwst-file.c
@@ -501,7 +501,7 @@ int dwstOfFileExt(
       lowestLow = cuInfo->low;
     if( res!=DW_DLV_OK || !cuInfo->high )
     {
-      int hasLow = res==DW_DLV_OK;
+      int hasLow = res==DW_DLV_OK && cuInfo->low;
       if( !hasLow ) cuInfo->low = 0;
       cuInfo->high = 0;
 
@@ -578,7 +578,7 @@ int dwstOfFileExt(
               break;
             }
             if( code==DW_RLE_base_addressx || code==DW_RLE_base_address ||
-                debug_addr_unavailable )
+                debug_addr_unavailable || !lowpc )
               continue;
 
             if( !hasLow || lowpc<cuInfo->low )

commit d3622a16ede5895306d009dc06bb6308e41d298f

fix regression of range base
The problem was introduced in 395411b (version 2.3).

src/dwst-file.c
@@ -433,6 +433,7 @@ typedef struct cu_info
 {
   Dwarf_Off offs;
   Dwarf_Addr low,high;
+  Dwarf_Addr base;
   Dwarf_Line *lines;
   Dwarf_Signed lineCount;
   Dwarf_Line_Context lineContext;
@@ -503,6 +504,7 @@ int dwstOfFileExt(
     cuInfo->low = 0;
     cuInfo->high = 0;
     int res = dwarf_lowhighpc( die,&cuInfo->low,&cuInfo->high );
+    cuInfo->base = res==DW_DLV_OK ? cuInfo->low : 0;
     if( res!=DW_DLV_OK || !cuInfo->high )
     {
       int hasLow = res==DW_DLV_OK && cuInfo->low;
@@ -520,7 +522,7 @@ int dwstOfFileExt(
         if( version<=4 )
         {
           int i;
-          Dwarf_Addr base = cuInfo->low;
+          Dwarf_Addr base = cuInfo->base;
           cuInfo->ranges = malloc( rangeCount*sizeof(range_t) );
           if( !cuInfo->ranges ) rangeCount = 0;
           for( i=0; i<rangeCount; i++ )
@@ -717,7 +719,7 @@ int dwstOfFileExt(
 
           if( (int)srcfileno+cuInfo->fileno_offs<=fileCount )
           {
-            inline_info ii = { ptr,cuInfo->low,
+            inline_info ii = { ptr,cuInfo->base,
               files,fileCount,callbackFunc,callbackFuncW,callbackContext,
               ptrOrig,(int)srcfileno+cuInfo->fileno_offs,lineno,columnno,
               cuInfo->fileno_offs };